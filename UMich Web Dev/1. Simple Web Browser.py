c798b1b329ee1ee68917dfe80afc5a20cbfa3d31
Shivesh Prakash <64745558+Shivesh777@users.noreply.github.com>
2022-11-15 00:48:43 -0500

    Web Dev 1

---
+++ UMich Web Dev/1. Simple Web Browser.py
@@ -0,0 +1,16 @@
+"""Building a simple web browser in Python"""
+import socket
+
+
+mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mysock.connect(('data.pr4e.org', 80))
+cmd = 'GET http://data.pr4e.otg/page1.htm HTTP/1.0\r\n\r\n'.encode()
+mysock.send(cmd)
+
+while True:
+    data = mysock.recv(512)
+    if len(data) < 1:
+        break
+    print(data.decode(), end='')
+
+mysock.close()
